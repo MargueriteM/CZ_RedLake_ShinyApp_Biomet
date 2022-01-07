0c4980241aa29a6cc8fd1a0edaf81f85e056f10f
Marguerite Mauritz <mauritzmarguerite@gmail.com>
2022-01-07 16:12:07 -0700

    Create shiny app to check Red Lake biomet through web interface.

--- app.R
+++ app.R
@@ -6,75 +6,6 @@
 # Created: 11 August 2021
 # Last update: 7 Jan 2022
 
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Red Lake Playa Biomet Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        # dateRangeInput("daterange", "Date range:",
-        #                start = min(biomet.long$date),
-        #                end   = max(biomet.long$date),
-        #                min = min(biomet.long$date),
-        #                max = max(biomet.long$date)),
-        
-        selectInput("variable1", "Biomet variable 1:",
-                    c("Atmospheric Pressure" = "^PA",
-                      "Soil Moisture" = "^SWC",
-                      "Air Temperature" = "^TA",
-                      "Soil Temperature" = "^TS",
-                      "Canopy Temperature" ="^TC",
-                      "Rainfall" =  "^P_RAIN",
-                      "Leaf wetness" = "^LWS",
-                      "Longwave Radiation" = "^LWIN|^LWOUT",
-                      "Shortwave Radiation" = "^SWIN|^SWOUT",
-                      "Albedo" = "^ALB",
-                      "Global Radiation & PAR" = "^RG|^PPFD",
-                      "Net Radiation" = "^RN")),
-        
-        selectInput("variable2", "Biomet variable 2:",
-                    c("Atmospheric Pressure" = "^PA",
-                      "Soil Moisture" = "^SWC",
-                      "Air Temperature" = "^TA",
-                      "Soil Temperature" = "^TS",
-                      "Canopy Temperature" ="^TC",
-                      "Rainfall" =  "^P_RAIN",
-                      "Leaf wetness" = "^LWS",
-                      "Longwave Radiation" = "^LWIN|^LWOUT",
-                      "Shortwave Radiation" = "^SWIN|^SWOUT",
-                      "Albedo" = "^ALB",
-                      "Global Radiation & PAR" = "^RG|^PPFD",
-                      "Net Radiation" = "^RN")),
-        
-        selectInput("variable3", "Radiation Components:",
-                    c("Longwave Radiation" = "^LWIN|^LWOUT",
-                      "Shortwave Radiation" = "^SWIN|^SWOUT",
-                      "Albedo" = "^ALB",
-                      "Global Radiation & PAR" = "^RG|^PPFD",
-                      "Net Radiation" = "^RN")),
-        
-        radioButtons("ax.scales", "Axis scales:",
-                     c("Fixed" = "fixed", 
-                       "Free x" = "free_x",
-                       "Free y" = "free_y",
-                       "Free x & y" = "free"))
-        
-       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("plot1"),
-         plotOutput("plot2"),
-         plotOutput("plot3")
-      )
-   )
-)
-
 # Define server logic required to draw a histogram
 # load libraries and data
 # load libraries
@@ -100,6 +31,11 @@ biomet <- read.table("CR3000 Red Lake Remote Connect_Biomet.dat", sep=",", dec="
 # change data to long format
 biomet.long <- biomet %>%
   pivot_longer(!c(TIMESTAMP,RECORD), names_to="variable",values_to="value") %>%
+  mutate(TIMESTAMP = ymd_hms(TIMESTAMP),
+         date=as.Date(TIMESTAMP))
+
+wind <- biomet %>%
+  select(TIMESTAMP, WS_16_33_1_1_1, WD_20_35_1_1_1) %>%
   mutate(TIMESTAMP = ymd_hms(TIMESTAMP),
          date=as.Date(TIMESTAMP))
 
@@ -150,58 +86,166 @@ plot.titles <- data.frame(selection1 = c("^PA",
                                    "Net Radiation"))
 
 
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- 
+  navbarPage("Red Lake Playa Biomet Data",
+             tabPanel("Overview",
+               "  Welcome!
+               This dashboard shows data collected by the Dryland Critical Zone Project at Red Lake Playa.
+                      
+    Optional Photo display: found in Dryland CZ/Data/Carbon/Tower Data/JER_Playa: 20211023_RedLake_SiteView.jpg"
+                    
+             ),
+             tabPanel("Plot timeseries data",
+             
+  fluidPage(
+   
+   # Sidebar with input options 
+   sidebarLayout(
+      sidebarPanel(
+         dateRangeInput("daterange", "Date range:",
+                        start = min(biomet.long$date),
+                        end   = max(biomet.long$date),
+                        min = min(biomet.long$date),
+                        max = max(biomet.long$date)),
+        
+        selectInput("variable1", "Biomet variable 1:",
+                    c("Atmospheric Pressure" = "^PA",
+                      "Soil Moisture" = "^SWC",
+                      "Air Temperature" = "^TA",
+                      "Soil Temperature" = "^TS",
+                      "Canopy Temperature" ="^TC",
+                      "Rainfall" =  "^P_RAIN",
+                      "Leaf wetness" = "^LWS",
+                      "Longwave Radiation" = "^LWIN|^LWOUT",
+                      "Shortwave Radiation" = "^SWIN|^SWOUT",
+                      "Albedo" = "^ALB",
+                      "Global Radiation & PAR" = "^RG|^PPFD",
+                      "Net Radiation" = "^RN")),
+        
+        selectInput("variable2", "Biomet variable 2:",
+                    c("Atmospheric Pressure" = "^PA",
+                      "Soil Moisture" = "^SWC",
+                      "Air Temperature" = "^TA",
+                      "Soil Temperature" = "^TS",
+                      "Canopy Temperature" ="^TC",
+                      "Rainfall" =  "^P_RAIN",
+                      "Leaf wetness" = "^LWS",
+                      "Longwave Radiation" = "^LWIN|^LWOUT",
+                      "Shortwave Radiation" = "^SWIN|^SWOUT",
+                      "Albedo" = "^ALB",
+                      "Global Radiation & PAR" = "^RG|^PPFD",
+                      "Net Radiation" = "^RN")),
+        
+        selectInput("variable3", "Radiation Components:",
+                    c("Longwave Radiation" = "^LWIN|^LWOUT",
+                      "Shortwave Radiation" = "^SWIN|^SWOUT",
+                      "Albedo" = "^ALB",
+                      "Global Radiation & PAR" = "^RG|^PPFD",
+                      "Net Radiation" = "^RN")),
+        
+        radioButtons("ax.scales", "Axis scales:",
+                     c("Fixed" = "fixed", 
+                       "Free x" = "free_x",
+                       "Free y" = "free_y",
+                       "Free x & y" = "free"))
+        
+       ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotOutput("plot1"),
+        plotOutput("plot2"),
+        plotOutput("plot3")
+      )
+   )
+)
+),
+
+tabPanel("Windrose",
+         fluidPage(
+           
+           # Sidebar with input options 
+           #fluidRow(
+           # dateRangeInput("daterange", "Date range:",
+           #                start = min(biomet.long$date),
+           #                end   = max(biomet.long$date),
+           #                min = min(biomet.long$date),
+           #                max = max(biomet.long$date))),
+           
+           fluidRow(
+         plotOutput("plot.windrose"))
+  )
+         )
+)
+
+
 server <- function(input, output) {
    
+  # create plots to graph
+  # make only date filtering parts reactive
+  
+  # create a date-filtered object
+  
    output$plot1 <- renderPlot({
-     c <-  biomet.long %>%
-       filter(str_detect(variable,input$variable1))# &
-               # (date >= input$daterange[[1]] & date <= input$daterange[[2]])) 
+     c1 <-  biomet.long %>%
+       filter(str_detect(variable,input$variable1) &
+                (date >= input$daterange[[1]] & date <= input$daterange[[2]])) 
      
-     d <- plot.titles %>%
+     d1 <- plot.titles %>%
        filter(selection1 == input$variable1)
-     # setnames(c,input$variable1,"selected")
-     
+
      # plot
-     ggplot(c, aes(TIMESTAMP, value))+
+     ggplot(c1, aes(TIMESTAMP, value))+
        geom_line()+
-       labs(title = d$name, y=d$name)+
+       labs(title = d1$name, y=d1$name)+
        facet_grid(variable~., scales = input$ax.scales)+
        theme_bw()
     
    })
    
    output$plot2 <- renderPlot({
-     c <-  biomet.long %>%
-       filter(str_detect(variable,input$variable2))# &
-               # (date >= input$daterange[[1]] & date <= input$daterange[[2]])) 
+     c2 <-  biomet.long %>%
+       filter(str_detect(variable,input$variable2) &
+                (date >= input$daterange[[1]] & date <= input$daterange[[2]])) 
      
-     d <- plot.titles %>%
+     d2 <- plot.titles %>%
        filter(selection2 == input$variable2)
      # setnames(c,input$variable1,"selected")
      
      # plot
-     ggplot(c, aes(TIMESTAMP, value))+
+     ggplot(c2, aes(TIMESTAMP, value))+
        geom_line()+
-       labs(title = d$name, y=d$name)+
+       labs(title = d2$name, y=d2$name)+
        facet_grid(variable~., scales = input$ax.scales)+
        theme_bw()})
    
    output$plot3 <- renderPlot({
-     c <-  biomet.long %>%
-       filter(str_detect(variable,input$variable3))# &
-               # (date >= input$daterange[[1]] & date <= input$daterange[[2]])) 
+     c3 <-  biomet.long %>%
+       filter(str_detect(variable,input$variable3) &
+                (date >= input$daterange[[1]] & date <= input$daterange[[2]])) 
      
-     d <- plot.titles %>%
+     d3 <- plot.titles %>%
        filter(selection3 == input$variable3)
-     # setnames(c,input$variable1,"selected")
-     
+
      # plot
-     ggplot(c, aes(TIMESTAMP, value, colour=variable))+
+     ggplot(c3, aes(TIMESTAMP, value, colour=variable))+
        geom_line()+
-       labs(title = d$name, y=d$name)+
+       labs(title = d3$name, y=d3$name)+
        facet_grid(input$variable3~.)+
        theme_bw()+
        theme(legend.position = "bottom")})
+   
+   output$plot.windrose <-  renderPlot({
+     wind.plot <-  wind %>%
+       drop_na() %>%
+       filter((date >= input$daterange[[1]] & date <= input$daterange[[2]]))
+     
+       # plot wind variables in windrose
+       plot.windrose(wind.plot,wind.plot$WS_16_33_1_1_1, wind.plot$WD_20_35_1_1_1)+theme_bw()
+     })
 }
 
 # Run the application 
